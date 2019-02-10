6336ad02722c0e37d7fd0051d55c12ad9dd2ec58
melih sahin <melihsahin24@gmail.com>
2019-02-10 18:30:03 +0300

    new fields added in users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,11 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('surname');
             $table->string('email')->unique();
+            $table->string('address');
+            $table->string('phone');
+            $table->string('user_company');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
